a7a01fbeace9e2f2ce6f19f31f3da27626cbee41
Akhil <akhilu5042@gmail.com>
2022-02-26 18:52:09 +0530

    Welcome to FundooProject

---
+++ FundooNote/BusinessLayer/Interface/IUserBL.cs
@@ -0,0 +1,11 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+
+namespace BusinessLayer.Interface
+{
+    //Business Layer User interface
+    public interface IUserBL
+    {
+        public User Registration(UserRegistration userRegist);
+    }
+}
---
+++ FundooNote/RepositoryLayer/Interface/IUserRL.cs
@@ -0,0 +1,14 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    //Repo Layer User interface
+    public interface IUserRL
+    {
+        public User Registration(UserRegistration userRegist);
+    }
+}
